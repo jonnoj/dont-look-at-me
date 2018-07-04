7a8d7a0d8998cae6b0f1ff95245cd9726bbe1ef1
jonnoj <jonbarraza1337@gmail.com>
2018-07-03 21:13:33 -0700

    Quotes/pictures

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -50,7 +50,20 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/1.jpg"
+	"backgrounds/images/1.jpg",
+	"backgrounds/images/2.jpg",
+	"backgrounds/images/3.jpg",
+	"backgrounds/images/4.jpg",
+	"backgrounds/images/5.jpg",
+	"backgrounds/images/6.jpg",
+	"backgrounds/images/7.jpg",
+	"backgrounds/images/8.jpg",
+	"backgrounds/images/9.jpg",
+	"backgrounds/images/10.jpg",
+	"backgrounds/images/11.jpg",
+	"backgrounds/images/12.jpg",
+	"backgrounds/images/13.jpg",
+	"backgrounds/images/14.jpg",
 ];
 
 /*Random background images order?*/
@@ -100,6 +113,23 @@ var l_messagesEnabled = true;
 
 /*Enter your custom messages below*/
 var l_messages = [
+"I'm such a gamer that if you cutt me I bleed gfuel",
+"That moment when you change a 10 to a 6...",
+"When the water company runs out of money, they don't pump water; they pump bananas",
+"I'm too sober for this shit",
+"Yaall fuckers a tootsie pop, hard on the outside but soft as fuck on the inside",
+"When an error is clientside but you think its serverside so you wipe the server for no reason",
+"Bilk Mubbles",
+"Milk milk lemonade.........",
+"wait, i could put literally anything as a quote?",
+"i tried crashing the server, i didn't get banned",
+"Squirted out some blood juice",
+"If there is not 143 whoppers in this box ima sue",
+"Porn Involves Sex",
+"Is there something wrong with your forehead?",
+"Jon is 6'3 that wears size 14 shoes",
+"Is it getting dim in here? Nah I think I'm just high.",
+"THE SPANISH INQUIZITION",
 "Gotta do what you gotta do.",
 "Is that a loss meme?",
 "You know what thats for? Spanish.",
@@ -118,8 +148,6 @@ var l_messages = [
 "Nothings impossible, you're all just too weak.",
 "JADYN IS GAY CUNFURMED 2019",
 "My headphones just died - Milk every night",
-"NUMBER FIFTEEEEEEEEEN",
-"do you know da wae?",
 "Welcome to Milk RP ENJOY YOUR STAY BITCH",
 "Actual Good Quote",
 "Rape - Short word, long sentence.",
@@ -127,14 +155,11 @@ var l_messages = [
 "Can I be admin/mod - Every Squeeker ever",
 "When you actually feel like you want to die inside but then you can just join the server",
 "Pick up that can",
-"WUBALUBA DUB DUB",
 "Kill me please",
 "NICKOLODEAN GIRLS CANT LIVE WITHOUT EM WHEW",
 "That Moment when you get bonus but it's gives you a 0",
 "I eat more cocain to get drunk",
 "I'm not gay but $20 is $20",
-"Roblox is better tahan Mine crafyt !!11!1!!!1",
-"Minecraft IS BETtER THAn ROB LOX!!!!!!!1",
 "Request a Quote Via the Discord!",
 "Request a Quote Via the Discord!",
 "Request a Quote Via the Discord!",
